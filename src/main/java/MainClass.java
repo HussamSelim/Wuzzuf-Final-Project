6930f4e92132d08b606b730b39d447ecbbfc6b69
MIDO <MIDO@169.254.113.185>
2021-07-04 16:42:58 +0200

    - Adding the Smile lib to the POM. - Added Methods class containing the old main class methods + required Smile methods. - Adding input from user to continue to further steps in the program(PRESS ENTER IN THE OUTPUT SECTION TO CONTINUE!). - Renamed WuzzufPOJO to Job and adding setters and getters for further purposes. - Doing 1st,2nd and 11th steps in the requirements using smile(waiting for Amr's response on the data viewing to finish 3rd step).

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,9 +1,8 @@
-import java.awt.Color;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;import java.util.stream.Collector;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
@@ -12,156 +11,115 @@ import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.knowm.xchart.CategoryChart;
-import org.knowm.xchart.CategoryChartBuilder;
-import org.knowm.xchart.PieChart;
-import org.knowm.xchart.PieChartBuilder;
-import org.knowm.xchart.SwingWrapper;
-import org.knowm.xchart.style.Styler;
+import smile.data.DataFrame;
+import smile.data.vector.IntVector;
 
 
 public class MainClass {
 
 	public static void main(String[] args) {
-		Logger.getLogger ("org").setLevel (Level.ERROR);
-		//Create a Spark conext
-		SparkConf conf = new SparkConf().setAppName("Wuzzuf").setMaster("local[3]");
-		JavaSparkContext context= new JavaSparkContext(conf);
-		// LOAD DATASETS
-		JavaRDD<String> WuzzufDataSet= context.textFile("E:\\iti\\Wuzzuf_Jobs.csv");
-		//Transformation
-		JavaRDD<String> WuzzufDataSetUpdated= WuzzufDataSet.distinct();
-		
-		//Transformation
-		JavaRDD<String> jobs= WuzzufDataSetUpdated
-			.map(MainClass::extractjobs)
-			.filter(StringUtils::isNotBlank);
-		
-		
-		JavaRDD<String> company= WuzzufDataSetUpdated
-				.map(MainClass::extractcompanies)
-				.filter(StringUtils::isNotBlank);
-		
-		JavaRDD<String> location= WuzzufDataSetUpdated
-				.map(MainClass::extractlocation)
-				.filter(StringUtils::isNotBlank);
-		
-		JavaRDD<String> skills= WuzzufDataSetUpdated
-				.map(MainClass::extractskills)
-				.filter(StringUtils::isNotBlank);
+            try {
+                // Creating dummy object to call functions from the Methods Class
+                Methods obj = new Methods();
+                //Using SMILE to print summary for the data
+                System.out.println ("=======Data Summary=========");
+                DataFrame jobSM = obj.readCSV ("F:\\Iti\\Foundation period\\Java & UML programming\\Wuzzuf_Jobs.csv");
+                System.in.read();
+                System.out.println ("=======Data Structure=========");
+                System.out.println (jobSM.structure ());
+                System.in.read();
+                jobSM = jobSM.merge (IntVector.of ("YearsExpValues",
+                obj.encodeCategory (jobSM, "YearsExp")));
 
-		
-		//counting Companies
-		Map<String,Long> companiesCount= company.countByValue();
-		Map<String, Long> sortedCompany= companiesCount.entrySet().stream()
-		.sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-		.collect(Collectors.toMap(
-				Map.Entry::getKey,
-				Map.Entry::getValue,
-				(oldValue, newValue)-> oldValue, LinkedHashMap::new));
-//		for(Map.Entry<String, Long>entry:sortedCompany.entrySet()) { System.out.println(entry.getKey()+':'+entry.getValue());}	
-		
-		// Plotting the Pie chart for the 5 most publishing companies on Wuzzuf
-		MainClass obj= new MainClass();
-//		obj.pieChart(sortedCompany);
-		
-		//Counting Jobs
-		Map<String,Long> jobsCount= jobs.countByValue();
-		Map<String, Long> sortedJobs= jobsCount.entrySet().stream()
-		.sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-		.collect(Collectors.toMap(
-				Map.Entry::getKey,
-				Map.Entry::getValue,
-				(oldValue, newValue)-> oldValue, LinkedHashMap::new));
-		ArrayList<String> jobKeys= new ArrayList<String>(sortedJobs.keySet());
-		ArrayList<Long> jobValues= new ArrayList<Long>(sortedJobs.values());
-		
-		ArrayList<String> first8JobKeys= (ArrayList<String>) jobKeys.stream().limit(8).collect(Collectors.toList());
-		ArrayList<Long> first8JobValues= (ArrayList<Long>) jobValues.stream().limit(8).collect(Collectors.toList());
-		//Calling graphJobPopularity to plot the bar chart
-//		obj.graphJobPopularity(first8JobKeys, first8JobValues);
-//		jobKeys.forEach(x->System.out.println(x));
-//		jobValues.forEach(x->System.out.println(x));
-		
-		
-		
-		// Counting Locations
-		Map<String,Long> locationCount= location.countByValue();
-		Map<String, Long> sortedLocation= locationCount.entrySet().stream()
-		.sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-		.collect(Collectors.toMap(
-				Map.Entry::getKey,
-				Map.Entry::getValue,
-				(oldValue, newValue)-> oldValue, LinkedHashMap::new));
-		ArrayList<String> locationKeys= new ArrayList<String>(sortedLocation.keySet());
-		ArrayList<Long> locationValues= new ArrayList<Long>(sortedLocation.values());
-		
-		ArrayList<String> first8LocationKeys= (ArrayList<String>) locationKeys.stream().limit(8).collect(Collectors.toList());
-		ArrayList<Long> first8LocationValues= (ArrayList<Long>) locationValues.stream().limit(8).collect(Collectors.toList());
-		
-		obj.graphPopularAreas(first8LocationKeys, first8LocationValues);
-	}
+                System.out.println ("=======Encoding YearsExp column Data==============");
+                System.out.println (jobSM.structure ());
+                System.out.println (jobSM);
+                System.in.read();
+                //Create a Spark conext
+                Logger.getLogger ("org").setLevel (Level.ERROR);
+                SparkConf conf = new SparkConf().setAppName("Wuzzuf").setMaster("local[3]");
+                JavaSparkContext context= new JavaSparkContext(conf);
+                // LOAD DATASETS
+                JavaRDD<String> WuzzufDataSet= context.textFile("F:\\Iti\\Foundation period\\Java & UML programming\\Wuzzuf_Jobs.csv");
+                //Transformation
+                JavaRDD<String> WuzzufDataSetUpdated= WuzzufDataSet.distinct();
+                
+                //Transformation
+                JavaRDD<String> jobs= WuzzufDataSetUpdated
+                        .map(Methods::extractjobs)
+                        .filter(StringUtils::isNotBlank);
+                
+                
+                JavaRDD<String> company= WuzzufDataSetUpdated
+                        .map(Methods::extractcompanies)
+                        .filter(StringUtils::isNotBlank);
+                
+                JavaRDD<String> location= WuzzufDataSetUpdated
+                        .map(Methods::extractlocation)
+                        .filter(StringUtils::isNotBlank);
+                
+                JavaRDD<String> skills= WuzzufDataSetUpdated
+                        .map(Methods::extractskills)
+                        .filter(StringUtils::isNotBlank);
+                
+                
+                //Counting Companies
+                Map<String,Long> companiesCount= company.countByValue();
+                Map<String, Long> sortedCompany= companiesCount.entrySet().stream()
+                        .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                        .collect(Collectors.toMap(
+                                Map.Entry::getKey,
+                                Map.Entry::getValue,
+                                (oldValue, newValue)-> oldValue, LinkedHashMap::new));
+		System.out.println("Company               : Frequancy of Company    ");
+                sortedCompany.forEach((k, v) -> System.out.println(k +"  :  "+v));
+                System.in.read();
+            // Plotting the Pie chart for the 5 most publishing companies on Wuzzuf
 
-	public static String extractjobs(String job) {
-        try {
-            return job.split (",")[0];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            return "";
-        }
-	}
-	public static String extractcompanies(String companies) {
-        try {
-            return companies.split (",")[1];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            return "";
-        }
-	}
-	public static String extractlocation(String location) {
-        try {
-            return location.split (",")[2];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            return "";
-        }
-	}
-	public static String extractskills(String skills) {
-        try {
-            return skills.split (",")[7];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            return "";}
-        }
-        
-        public void pieChart(Map result) {
-        PieChart chart = new PieChartBuilder().width (800).height (600).title (getClass().getSimpleName()).build ();
-        // Customize Chart
-        Color[] sliceColors= new Color[]{new Color (180, 68, 50), new Color (130, 105, 120), new Color (80, 143, 160)};
-        chart.getStyler().setSeriesColors(sliceColors);
-        
-        chart.addSeries((String) result.keySet().toArray()[0], (Number) result.values().toArray()[0]);
-    	chart.addSeries((String) result.keySet().toArray()[1], (Number) result.values().toArray()[1]);
-    	chart.addSeries((String) result.keySet().toArray()[2], (Number) result.values().toArray()[2]);
-    	chart.addSeries((String) result.keySet().toArray()[3], (Number) result.values().toArray()[3]);
-    	chart.addSeries((String) result.keySet().toArray()[4], (Number) result.values().toArray()[4]);
-    	new SwingWrapper(chart).displayChart();
-    	   
-       }
-        public static void graphJobPopularity(List<String> jobKeys, List<Long> jobValues ) {
-        CategoryChart chart = new CategoryChartBuilder().width (1024).height (768).title ("Most Popular Job Titles").xAxisTitle("Jobs").yAxisTitle("Popularity").build();
-		chart.getStyler().setLegendPosition(Styler.LegendPosition.OutsideE);
-		chart.getStyler().setHasAnnotations(true);
-		chart.getStyler().setStacked(true);
-		chart.addSeries("Jobs Popularity",jobKeys, jobValues);
-		
-		new SwingWrapper(chart).displayChart();
-        }
-        
-        public static void graphPopularAreas(List<String> locationKeys, List<Long> locationValues ) {
-            CategoryChart chart = new CategoryChartBuilder().width (1024).height (768).title ("Most Popular Job Titles").xAxisTitle("Jobs").yAxisTitle("Popularity").build();
-    		chart.getStyler().setLegendPosition(Styler.LegendPosition.OutsideE);
-    		chart.getStyler().setHasAnnotations(true);
-    		chart.getStyler().setStacked(true);
-    		chart.addSeries("Jobs Popularity",locationKeys, locationValues);
-    		
-    		new SwingWrapper(chart).displayChart();
+		obj.pieChart(sortedCompany);
+                System.in.read();
+                //Counting Jobs
+                Map<String,Long> jobsCount= jobs.countByValue();
+                Map<String, Long> sortedJobs= jobsCount.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldValue, newValue)-> oldValue, LinkedHashMap::new));
+                ArrayList<String> jobKeys= new ArrayList<String>(sortedJobs.keySet());
+                ArrayList<Long> jobValues= new ArrayList<Long>(sortedJobs.values());
+
+                ArrayList<String> first8JobKeys= (ArrayList<String>) jobKeys.stream().limit(8).collect(Collectors.toList());
+                ArrayList<Long> first8JobValues= (ArrayList<Long>) jobValues.stream().limit(8).collect(Collectors.toList());
+                //Calling graphJobPopularity to plot the bar chart
+                obj.graphJobPopularity(first8JobKeys, first8JobValues);
+                System.in.read();
+                System.out.println("Job               : Frequancy of job    ");
+                sortedJobs.forEach((k, v) -> System.out.println(k +"  :  "+v));
+                //jobValues.forEach(x->System.out.println(x));
+
+
+
+                // Counting Locations
+//		Map<String,Long> locationCount= location.countByValue();
+//		Map<String, Long> sortedLocation= locationCount.entrySet().stream()
+//		.sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+//		.collect(Collectors.toMap(
+//				Map.Entry::getKey,
+//				Map.Entry::getValue,
+//				(oldValue, newValue)-> oldValue, LinkedHashMap::new));
+//		ArrayList<String> locationKeys= new ArrayList<String>(sortedLocation.keySet());
+//		ArrayList<Long> locationValues= new ArrayList<Long>(sortedLocation.values());
+//		
+//		ArrayList<String> first8LocationKeys= (ArrayList<String>) locationKeys.stream().limit(8).collect(Collectors.toList());
+//		ArrayList<Long> first8LocationValues= (ArrayList<Long>) locationValues.stream().limit(8).collect(Collectors.toList());
+//		
+//		obj.graphPopularAreas(first8LocationKeys, first8LocationValues);
+            } catch (IOException ex) {
+                java.util.logging.Logger.getLogger(MainClass.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
             }
-	}
+    }
+
+	
+}
 
